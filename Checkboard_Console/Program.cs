ad5f4e7847047b2dbc1f19b83872730f984e0605
Pieter Beernink <60547491+pieterbnl@users.noreply.github.com>
2022-01-13 17:00:56 +0100

    Add project files.

---
+++ Checkboard_Console/Program.cs
@@ -0,0 +1,19 @@
+﻿// Part 1. Make a simple console app. Make it say what you want.
+Console.WriteLine("**********************");
+Console.WriteLine("Draw me a checkerboard");
+Console.WriteLine("**********************");
+
+// Part 2. Make it accept user input and have it print out something based on that input.
+Console.WriteLine("Before we start. What's your name?");
+
+
+/*Part 3: Make a checkerboard in the console, using nested for loops.
+As addition:
+- allow the user to select the characters for the checkerboard
+- allow the user input the number of rows and columns
+- add a coloring scheme to the output
+- check for (in)valid input*/
+
+/*BONUS:
+- implement a selection menu
+- refacture your code, taking in account SOLID principles*/
